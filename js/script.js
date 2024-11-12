1856989d7c52aaf0d981e0ee664748868834107b
Renzo Bardalez <r.bardalez@outlook.com>
2024-11-12 01:53:52 -0500

    Entregable 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /* Mostar div por cada cuenta bancaria */
 // Obtenemos el localStorage para validar la cantidad de cuentas existentes
 const account = JSON.parse(localStorage.getItem("account")) || [];
-
 const bankContainer = document.getElementById("account-container-result")
 
 account.forEach((elm) => {
@@ -12,8 +11,9 @@ div.innerHTML =
     `
     <h4> Bank: ${elm.accountBank}</h4>
     <p> Currency: ${elm.accountCurrency}</p>
-    <p> Balance: $ ${elm.accountBalance}</p>
+    <p> Balance: ${elm.accountBalance}</p>
 `
+/* VER COMO AGREGAR EL SIBOLO DEL ARRAY CURRENCY */
 console.log(div)
 bankContainer.appendChild(div)
 })

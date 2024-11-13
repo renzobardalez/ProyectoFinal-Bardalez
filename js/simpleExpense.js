821d4b3525b6c2ac324df5dbb41c4dc55da9781f
Renzo Bardalez <r.bardalez@outlook.com>
2024-11-13 00:49:05 -0500

    Entregable 3

--- js/simpleExpense.js
+++ js/simpleExpense.js
@@ -1,12 +1,53 @@
 /* Opción dinámica de bank */
 // Selccionamos el elemento select
-const simpleExpenseBank = document.getElementById("simpleExpenseBank");
+const simpleExpenseAccount = document.getElementById("simpleExpenseAccount");
 // Cargamos el array
-const bankAccount = JSON.parse(localStorage.getItem("bankAccount")) || [];
+const account = JSON.parse(localStorage.getItem("account")) || [];
 // Iteramos sobre cada item
-bankAccount.forEach((elm) => {
+account.forEach((elm) => {
     const option = document.createElement("option");
     option.value = elm.id;
-    option.textContent = elm.bankName;
-    simpleExpenseBank.appendChild(option);    
+    option.textContent = elm.accountName; // VER COMO AGREGAR EL NOMBRE DEL BANCO
+    simpleExpenseAccount.appendChild(option);    
+});
+
+/* Modificación de balance de cuenta */
+document.getElementById("saveSimpleExpense").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que la página se recargue
+    /* Verificación del array */
+    const account = JSON.parse(localStorage.getItem("account")) || [];
+    /* Obtenemos los valores de la página */
+    const simpleExpenseAccountId = simpleExpenseAccount.value;
+    const simpleExpenseAmmount = document.getElementById("simpleExpenseAmmount").value;
+    if (!simpleExpenseBank || isNaN(simpleExpenseAmmount)
+    ){
+        const simpleExpenseMessage = document.getElementById("simpleExpenseMessage");
+        simpleExpenseMessage.textContent = "Por favor ingrese valores válidos.";
+        setTimeout(() => {
+            window.location.reload();
+        },1500);
+        return;
+    }
+    const currentBalance = account.accountBalance - simpleExpenseAmmount
+    /* Modificación del array */
+    let newBalance = {
+        accountBalance: parseFloat(currentBalance)
+    };
+    
+    account.push(newAccount);
+    localStorage.setItem("account", JSON.stringify(account));
+    localStorage.setItem("accountIdCounter", newId +1);
+    /* Mensaje de éxito */
+    const accountMessage = document.getElementById("accountMessage");
+    accountMessage.textContent="Registro grabado con éxito.";
+    /* Reseteamos los campos */
+    document.getElementById("accountBank").value="";
+    document.getElementById("accountCurrency").value="";
+    document.getElementById("accountName").value="";
+    document.getElementById("accountNumber").value="";
+    document.getElementById("accountDescription").value="";
+    document.getElementById("accountBalance").value="";
+    setTimeout(() => {
+        window.location.reload();
+    },1500);
 });
